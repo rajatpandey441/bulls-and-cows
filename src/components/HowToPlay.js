89901b5619865f667454b82d10b337910565b413
Shaileshalluri <shaileshalluri@gmail.com>
2023-10-15 19:23:04 -0400

    Changes to HowtoPlay added

--- src/components/HowToPlay.js
+++ src/components/HowToPlay.js
@@ -32,9 +32,15 @@ const HowToPlay = () => {
             <AlertDialogBody>
               Discover the hidden code! Click, or use arrow keys, to change your
               guess in each box. Bulls = correct code, correct position. Cows =
-              correct code, wrong position. For example,if hidden code is 2145 and 
-              the number you guessed is 1246 then bulls would be 1 and cows would be 
-              2.
+              correct code, wrong position. For example, if the hidden code is "2145,"
+              and you guess "1246," the feedback would be: 
+              Bulls : 1, the "1" in your guess is in the correct position.
+              Cows: 2, the "2" and "4" in your guess are correct but in the wrong positions.
+              <br/>
+              <br/>
+              You can use this feedback to make educated guesses and gradually narrow down 
+              the possible combinations until you correctly guess the hidden code. 
+              It's a fun and challenging logic puzzle game!
             </AlertDialogBody>
 
             <AlertDialogFooter>
